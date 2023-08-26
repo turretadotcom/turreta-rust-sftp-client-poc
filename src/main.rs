0000d0d1d7bf96eb5952a660675f872d4a584807
karl San Gabriel <turretasolutions@gmail.com>
2023-08-26 13:12:15 +0800

    Initial dev codes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::TcpStream;
-use ssh2::Session;
+use ssh2::{DisconnectCode, Session, Sftp};
 use std::path::Path;
 use chrono::{Local, FixedOffset};
 use actix_web::{web, App, HttpServer};
@@ -23,8 +23,12 @@ async fn main() -> std::io::Result<()> {
         let o = q.split(partners_list.as_str());
         let y:Vec<_> = o.into_iter().collect();
 
+
+
         print!("{:?}", y);
         // Pred partners SFTP dirs
+
+
 
         /*
 
@@ -60,6 +64,12 @@ async fn main() -> std::io::Result<()> {
         let offset = Some(FixedOffset::east_opt(0).unwrap()).unwrap();
 
         loop {
+
+            let o: Session = get_tcp_stream_session();
+            let sftp = o.sftp().unwrap();
+
+            println!("Connected to SFTP");
+
             let mut upcoming = schedule.upcoming(offset).take(1);
             actix_rt::time::sleep(Duration::from_millis(500)).await;
             let local = &Local::now();
@@ -74,6 +84,9 @@ async fn main() -> std::io::Result<()> {
                     println!("{:?}",result);
                 }
             }
+
+            o.disconnect(Some(DisconnectCode::ByApplication), "Done", Option::None).expect("TODO: panic message");
+            println!("Disconnected from SFTP");
         }
     });
     HttpServer::new(|| {
@@ -85,6 +98,53 @@ async fn main() -> std::io::Result<()> {
         .await
 }
 
+fn get_tcp_stream_session() -> Session {
+
+    let sftp_endpoint_url = std::env::var("SFTP_SERVER_ENDPOINT_URL").expect("SFTP_SERVER_ENDPOINT_URL must be set.");
+    let sftp_username = std::env::var("SFTP_SERVER_USERNAME").expect("SFTP_SERVER_USERNAME must be set.");
+    let sftp_user_password = std::env::var("SFTP_SERVER_USERPWD").expect("SFTP_SERVER_USERPWD must be set.");
+
+    let tcp = TcpStream::connect(sftp_endpoint_url.as_str()).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_password(sftp_username.as_str(), sftp_user_password.as_str()).unwrap();
+
+    sess
+}
+
+
+fn prepare_dir_structure_when_required() -> Session {
+
+    let sftp_endpoint_url = std::env::var("SFTP_SERVER_ENDPOINT_URL").expect("SFTP_SERVER_ENDPOINT_URL must be set.");
+    let sftp_username = std::env::var("SFTP_SERVER_USERNAME").expect("SFTP_SERVER_USERNAME must be set.");
+    let sftp_user_password = std::env::var("SFTP_SERVER_USERPWD").expect("SFTP_SERVER_USERPWD must be set.");
+
+    let tcp = TcpStream::connect(sftp_endpoint_url.as_str()).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_password(sftp_username.as_str(), sftp_user_password.as_str()).unwrap();
+
+    sess
+}
+
+fn get_sftp_client_instance() -> Sftp {
+
+    let sftp_endpoint_url = std::env::var("SFTP_SERVER_ENDPOINT_URL").expect("SFTP_SERVER_ENDPOINT_URL must be set.");
+    let sftp_username = std::env::var("SFTP_SERVER_USERNAME").expect("SFTP_SERVER_USERNAME must be set.");
+    let sftp_user_password = std::env::var("SFTP_SERVER_USERPWD").expect("SFTP_SERVER_USERPWD must be set.");
+
+    let tcp = TcpStream::connect(sftp_endpoint_url.as_str()).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_password(sftp_username.as_str(), sftp_user_password.as_str()).unwrap();
+
+    let sftp = sess.sftp().unwrap();
+
+    sftp
+}
 fn main_temp() {
 
     let tcp = TcpStream::connect("127.0.0.1:9922").unwrap();
@@ -115,4 +175,6 @@ fn main_temp() {
         let ff2 = Path::new("/foo-home/file2.json");
         sftp.rename(ff, ff2, None).unwrap();
     }
+
+
 }
